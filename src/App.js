89a716c9d13d25014fa4f2ced2ccbb8bfdb34a96
Naman Kumar Sinha <naman.sinha@infostretch.com>
2021-05-26 19:38:47 +0530

    hustle graph w/o legend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Graph1 from "./Graph1";
-import Graph2 from "./Graph2";
-import Graph3 from "./Graph3";
 import Legend from "./Legend";
 import HustleGraph from "./HustleGraph";
 import responseData from "./updated-response.json";
@@ -14,9 +11,6 @@ function App() {
       <div className="container-parent">
         <div className="hustle">HUSTLE</div>
         <div className="container-child1">
-          {/* <Graph1 data={responseData.result.mental.moodData} />
-        <Graph2 data={responseData.result.mental.moodData}/>
-        <Graph3 data={responseData.result.mental.moodData} /> */}
           <HustleGraph data={mentalData} />
         </div>
         <div className="container-child2">
