b8d824ad7bada1a3d2c6644f41c86fe94e8e5672
Naman Kumar Sinha <naman.sinha@infostretch.com>
2021-05-27 01:32:33 +0530

    hustle graph finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Legend from "./Legend";
 import HustleGraph from "./HustleGraph";
 import responseData from "./updated-response.json";
 
 function App() {
   const { mentalData } = responseData.result.mental;
+  const COLORS = ["#FEEFB8", "#FEDC62", "#FFC700", "#CA9D00", "#9A7A07"];
+  const {
+    composureScorePercentage,
+    motivationScorePercentage,
+    concentrationScorePercentage,
+    energyScorePercentage,
+    confidenceScorePercentage,
+  } = mentalData;
+
+  const data = [
+    {
+      name: "composureScorePercentage",
+      value: composureScorePercentage,
+      alias: "Composure",
+    },
+    {
+      name: "motivationScorePercentage",
+      value: motivationScorePercentage,
+      alias: "Motivation",
+    },
+    {
+      name: "concentrationScorePercentage",
+      value: concentrationScorePercentage,
+      alias: "Concentration",
+    },
+    {
+      name: "energyScorePercentage",
+      value: energyScorePercentage,
+      alias: "Tirelessness",
+    },
+    {
+      name: "confidenceScorePercentage",
+      value: confidenceScorePercentage,
+      alias: "Confidence",
+    },
+  ];
+
   return (
     <>
       <div className="container-parent">
@@ -15,7 +50,18 @@ function App() {
         </div>
         <div className="container-child2">
           <div className="legend">
-            <Legend data={mentalData} />
+            <div>Score Breakdown</div>
+          </div>
+          <div className="list" >
+            {data.map((item, index) => {
+              return (
+                <div className="col" key={index}>
+                  <div className="dot" style ={{backgroundColor: COLORS[index]}}></div>
+                  <span> {`${item.value} %`}</span>
+                  <span> {item.alias} </span>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
